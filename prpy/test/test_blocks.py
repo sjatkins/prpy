3ccf6f9e7561ca7828e279d6d02245ac951a06d1
Samantha Atkins <sjatkins@gmail.com>
2022-05-31 01:01:45 -0600

    first successful MOB file parsing to blocks

--- prpy/test/test_blocks.py
+++ prpy/test/test_blocks.py
@@ -4,11 +4,20 @@ from prpy.parsing.utils import Block
 
 world_path = world_files.__path__._path[0]
 rooms_path = os.path.join(world_path, 'ROOM')
+mobs_path = os.path.join(world_path, 'MOB')
 
-def test_blocks():
-    path = os.path.join(rooms_path, 'Midgard.room')
+# def test_room_blocks():
+#     path = os.path.join(rooms_path, 'Midgard.room')
+#     with open(path) as f:
+#         lines = [l.strip() for l in f.readlines()]
+#         lines = [l for l in lines[1:] if l]
+#     blocks = Block.blocks_from_lines(lines)
+#     assert blocks
+
+def test_mob_blocks():
+    path = os.path.join(mobs_path, 'Arachnos.mob')
     with open(path) as f:
         lines = [l.strip() for l in f.readlines()]
-        lines = [l for l in lines[1:] if l]
+        lines = [l for l in lines if l]
     blocks = Block.blocks_from_lines(lines)
     assert blocks
